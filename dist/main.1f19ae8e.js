b0ab4627ab3af4a5b26636ab07969bc9f0777354
nphan03 <nphan03.145@gmail.com>
2021-11-26 22:04:22 -0800

    fix errors when press start multiple times after stop and interval's running

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -139,29 +139,33 @@ function start() {
   if (startTime == null) {
     //check that clock is not running. NOT RUNNING: get startTime
     startTime = new Date();
+  } else {
+    clearInterval(interval);
   }
 
   if (stopTime != null) {
     stopDuration += new Date() - stopTime;
+    stopTime = null;
   }
 
   interval = setInterval(counter, 10);
 }
 
 function stop() {
-  stopTime = new Date();
   clearInterval(interval);
+  stopTime = new Date();
 }
 
 function reset() {
+  clearInterval(interval);
   stopDuration = 0;
   startTime = null;
   stopTime = null;
   printTime(0, 0, 0, 0);
-  clearInterval(interval);
 }
 
 function counter() {
+  console.log("1");
   var currentTime = new Date();
   var timeLapse = new Date(currentTime - startTime - stopDuration);
   var millisec = timeLapse.getUTCMilliseconds(),
@@ -209,7 +213,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1414" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "21066" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
